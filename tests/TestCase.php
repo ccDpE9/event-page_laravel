23374fd06161e824f6f57c55cdb708bcf5d318f3
Kosta__ <z_eon00@protonmail.com>
2019-02-15 17:45:30 +0100

    Helper testing methods

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,6 +8,16 @@ abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
+    function create($class, $attributes = [])
+    {
+        return factory($class)->create($attributes);
+    }
+
+    function make($class, $attributes = [])
+    {
+        return factory($class)->make($attributes);
+    }
+
     protected function signIn($user=null)
     {
         $user = $user ?: create ('App\User');
