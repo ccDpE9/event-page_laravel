2a09beb21f0bb2d2c789278933b2ab304e58ed0d
Kosta__ <z_eon00@protonmail.com>
2019-04-21 16:52:30 +0200

    Removed voyager, for now

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@
 //Route::get('/concerts', 'ConcertsController@index')->name('concerts.index');
 
 Route::view('/{path?}', 'app');
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
