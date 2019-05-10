15d87b95fb3954d36b7cbd21c2cbbb493758ad2d
ccDpE9 <z_eon00@protonmail.com>
2019-05-10 22:32:49 +0200

    StripeProvider creates Stripe instance. And, it passes it into child components? Elements combines CardElements into a single group.

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,15 @@
+require("dotenv").config();
 import React, { Component } from "react";
+import { StripeProvider, Elements } from "react-stripe-elements";
 
-class App extends Component {
-};
+import CheckoutForm from "./components/CheckoutForm";
+
+const App = () => (
+  <StripeProvider apiKey={process.env.STRIPE_KEY}>
+    <Elements>
+      <CheckoutForm />
+    </Elements>
+  </StripeProvider>
+);
 
 export default App;
