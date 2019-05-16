94e15dac52aa34636b2db20e77f47caf76a41c33
ccDpE9 <z_eon00@protonmail.com>
2019-05-16 20:41:59 +0200

    Re-named, typos, role_id

--- database/migrations/2019_05_16_153559_create_roles_table.php
+++ database/migrations/2019_05_16_153559_create_roles_table.php
@@ -15,7 +15,7 @@ class CreateRolesTable extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('title');
+            $table->string('name');
             $table->string('slug')->unique();
             $table->jsonb('permission')->default('{}');
             $table->timestamps();
