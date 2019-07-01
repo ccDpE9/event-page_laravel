0e2a556dd8e52880578c97a4c7a85e63680797f9
ccDpE9 <z_eon00@protonmail.com>
2019-07-01 19:22:28 +0200

    Made create-related tests more compact.

--- tests/Feature/CreateConcertTest.php
+++ tests/Feature/CreateConcertTest.php
@@ -15,53 +15,32 @@ class CreateConcertTest extends TestCase
     {
         parent::setUp();
     }
-
-
-    public function fillable_fields_can_be_stored()
-    {
-        // - Tests mass asignment and fillable property
-        // - Basically tests whether the store() method works when everything is as expected
-        $concert = factory("App\Concert")->make()->toArray();
-        $concert["title"] = "Testing title";
-        $this->call("POST", route("concerts.store"), $concert);
-        $this->assertDatabaseHas("concerts", $concert);
-    }
-
-    /** @test */
-    public function guarded_fields_can_be_stored() 
-    {
     
-    }
 
     /** @test */
-    public function test_required_fields()
-    {
-        $concert = factory("App\Concert")->create([
-            "title" => "",
-        ]);
-        $this-->assertSessionHasErrors([
-            "title"
-        ]);
-    }
-
-    /** @test */
-    public function fields_are_stored_as_specified()
-    {
-        $concert = factory("App\Concert")->create();
-        $mapFields = array(); 
-        foreach($concert->toArray() as $field) {
-            $arr[$field] = $concert->{$field};
-        }
-        $response->assertJsonFragment($mapFields);
-    }
-
-    /** @test */
-    public function store_returns_newly_created_concert()
+    public function user_can_store_concert_and_get_it_as_response()
     {
         $concert = factory("App\Concert")->make()->toArray();
+        
         $this
             ->call("POST", route("concerts.store"), $concert)
-            ->assertJsonStructure($this->responseJsonStructure);
+            ->assertStatus(200)
+            ->assertJsonStructure($this->responseJsonStructure)
+            ->assertJsonFragment($concert);
+    }
+
+    /** @test */
+    public function trying_to_store_non_nullable_field_returns_an_error()
+    {
+        $concert = factory("App\Concert")->make()->toArray();
+
+        foreach($concert as $field => $value) {
+            $concert[$field] = null;
+            $this
+                ->call("POST", route("concerts.store"), $concert)
+                ->assertSessionHasErrors([$field]);
+        }
+
     }
 
     /** @test */
