7bca3e3541cae4b1746abcb3cb3f7e1b09c682e5
ccDpE9 <z_eon00@protonmail.com>
2019-07-12 12:58:41 +0200

    Made a middleware that checks whether the user is root or not. Placed it in-front of the register route, so that only root users can make new profiles.

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
@@ -10,3 +11,20 @@ Route::namespace("Api")->group(function () {
     Route::apiResource("concerts", "ConcertController");
     Gate::resource("concerts", "App\Policies\ConcertPolicy");
 });
+
+Route::group([
+    "prefix" => "auth"
+], function () {
+    Route::post("login", "Auth\LoginController@login")->name("login");
+    Route::post("register", "Auth\RegisterController@create")->name("register")->middleware("isRoot");
+    /*
+    Route::group([
+        "middleware" => "auth:api"
+    ], function() {
+        Route::get("logout", "Auth\Controller@logout");
+        Route::get("user", "Auth\Controller@user");
+    });
+     */
+});
+
+?>
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -4,11 +4,11 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class UserTest extends TestCase
 {
-
     use DatabaseMigrations;
 
     public function setUp(): void
@@ -16,48 +16,142 @@ class UserTest extends TestCase
         parent::setUp();
     }
 
-    // --- REGISTER
-
     /** @test */
-    public function user_can_register()
+    public function non_root_user_cannot_create_new_user()
     {
-        $this->post(route("user.store", [
+        $this->withoutExceptionHandling();
+
+        $response = $this->post(route("register"), [
+            "name" => "test",
+            "email" => "valid@email.com",
+            "password" => "test"
         ]);
-        $this->assertDatabaseHas("users", [
-            email => "testing email"
-        ]);
+
+        $response->assertJson([
+            "data" => "Unauthorized action."
+        ]); 
     }
 
     /** @test */
-    public function newly_created_users_are_unverified()
+    public function root_can_create_new_user()
     {
+        $user = factory("App\User")->states("root")->create();
+        $this->actingAs($user);
+        $newUser = [
+            "name" => "test",
+            "email" => "valid@email",
+            "password" => "test"
+        ];
+        $response = $this->post(route("register"), $newUser);
+        $this->assertContains(
+            $response,
+            $newUser
+        );
+
     }
-    
+
+    /** @test */
+    public function registration_requires_password_and_email()
+    {
+        $this
+            ->json("POST", "api/register")
+            ->assertJson([
+                "name" => ["The name field is required."],
+                "email" => ["The email field is required."],
+                "password" => ["The password field is required."],
+            ]);
+    }
+
+    /** @test */
+    public function registration_requires_password_confirmation()
+    {
+        $this
+            ->json("POST", "api/register", [
+                "name" => "Test",
+                "email" => "test@example.com",
+                "password" => "password",
+            ])
+            ->assertStatus(422)
+            ->assertJson([
+                "password" => ["The password confirmation does not match"]
+            ]);
+    }
+
+
     // --- LOGIN
+    
+    /** @test */
+    public function email_and_password_are_require()
+    {
+        $this->json("POST", "api/login")
+             ->assertStatus(200)
+             ->assertJson([
+                 "email" => ["The email field is required."],
+                 "password" => ["The password field is required."]
+             ]);
+    }
 
     /** @test */
-    public function user_can_log_in()
+    public function successfull_login()
     {
-        $user = factory(User::class)->create([
-            "password" => Hash::make("password")
+        $user = create("App\User", [
+            "email" => "test@example.com",
+            "password" => bcrypt("password"),
         ]);
-        $this->post("login", [
-            "email" => user.email,
-            "password" => "password"
-        ]);
-        $this->assertTrue(auth()->check());
-        $this->assertTrue($user->is(auth()->user()));
+
+        $this
+            ->json("POST", "api/login", [
+                "email" => "test@example.com",
+                "password" => bcrypt("password")
+            ])
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                "data" => [ "id", "name", "email", "api_token" ],
+            ]);
     }
 
-    // --- DELETE
+
+    // --- LOGOUT
 
     /** @test */
-    public function user_cannot_delete_other_peoples_profiles()
+    public function user_is_logged_out_properly()
     {
+        $user = create("App\User", ["email" => "test@example.com"]);
+        $token = $user->generateToken();
+        $headers = ["Authorization" => "Bearer $token"];
+
+        $this
+            ->json("GET", "/api/articles", [], $headers)
+            ->assertStatus(200);
+        $this
+            ->json("POST", "/api/logout", [], $headers)
+            ->assertStatus(200);
+
+        $user = User::find($user->id);
+
+        $this->assertEquals(null, $user->api_token);
     }
 
     /** @test */
-    public function authorized_user_can_delete_his_hers_profile()
+    public function user_is_logged_out_without_null_token()
     {
+        $user = create("App\User", ["email" => "test@example.com"]);
+        $token = $user->generateToken();
+        $headers = ["Authorization" => "Bearer $token"];
+
+        // Simulating logout
+        $user->api_token = null;
+        $user->save();
+
+        $this
+            ->json("GET", "/api/articles", [], $headers)
+            ->assertStatus(401);
     }
+
+    /** @test */
+    public function upcoming_near_concerts_notification()
+    {
+        // https://laravel.com/docs/5.8/mocking#mail-fake
+    }
+
 }
