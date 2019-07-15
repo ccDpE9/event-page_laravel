bf5a8d71770d51e39a75ce99ad9a276c60c90061
ccDpE9 <z_eon00@protonmail.com>
2019-07-15 16:08:25 +0200

    Testing login controller. Had to comment out root login in test's setUp() method. Dunno why, but it would bug out my login test. Will fix it next.

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -19,9 +19,15 @@ class UserTest extends TestCase
         $this->root = factory("App\User")->states("root")->create([
             "password" => Hash::make("testpass")
         ]);
+        /*
         $this->rootToken = $this->json("POST", route("login"), [
             "email" => $this->root["email"],
             "password" => "testpass"
+        ])->baseResponse->original["token"];
+         */
+
+        $this->admin = factory("App\User")->create([
+            "password" => Hash::make("adminpass"),
         ]);
     }
 
@@ -31,7 +37,7 @@ class UserTest extends TestCase
         $response = $this->post(route("register"), [
             "name" => "test",
             "email" => "valid@email.com",
-            "password" => "test"
+            "password" => "test",
             "password_confirmation" => "test"
         ]);
 
@@ -46,7 +52,7 @@ class UserTest extends TestCase
         $this
             ->actingAs($this->root, "api")
             ->withHeaders([
-                "Authorization" => "Bearer ".$this->rootToken->baseResponse->original["token"],
+                "Authorization" => "Bearer ".$this->rootToken,
                 "Accept" => "application/json",
             ])
             ->json("POST", route("register"), [
@@ -65,12 +71,12 @@ class UserTest extends TestCase
         $this
             ->actingAs($this->root, "api")
             ->withHeaders([
-                "Authorization" => "Bearer ".$this->rootToken->baseResponse->original["token"],
+                "Authorization" => "Bearer ".$this->rootToken,
                 "Accept" => "application/json",
             ])
             ->json("POST", route("register"), [
                 "email" => "validtwo@email.com",
-                "password" => "testingyz"
+                "password" => "testingyz",
                 "password_confirmation" => "testingyz"
             ])
             ->assertJsonFragment([
@@ -84,12 +90,12 @@ class UserTest extends TestCase
         $this
             ->actingAs($this->root, "api")
             ->withHeaders([
-                "Authorization" => "Bearer ".$this->rootToken->baseResponse->original["token"],
+                "Authorization" => "Bearer ".$this->rootToken,
                 "Accept" => "application/json",
             ])
             ->json("POST", route("register"), [
                 "name" => "userone",
-                "password" => "testingyz"
+                "password" => "testingyz",
                 "password_confirmation" => "testingyz"
             ])
             ->assertJsonFragment([
@@ -103,7 +109,7 @@ class UserTest extends TestCase
         $this
             ->actingAs($this->root, "api")
             ->withHeaders([
-                "Authorization" => "Bearer ".$this->rootToken->baseResponse->original["token"],
+                "Authorization" => "Bearer ".$this->rootToken,
                 "Accept" => "application/json",
             ])
             ->json("POST", route("register"), [
@@ -121,7 +127,7 @@ class UserTest extends TestCase
         $this
             ->actingAs($this->root, "api")
             ->withHeaders([
-                "Authorization" => "Bearer ".$this->rootToken->baseResponse->original["token"],
+                "Authorization" => "Bearer ".$this->rootToken,
                 "Accept" => "application/json",
             ])
             ->json("POST", route("register"), [
@@ -138,31 +144,38 @@ class UserTest extends TestCase
     // --- LOGIN
 
     /** @test */
-    public function login_requires_email_and_password()
+    public function email_field_is_required_on_login()
     {
-        $this->post(route("login"))
-             ->assertStatus(302)
-             ->assertJson([
+        $this
+             ->json("POST", route("login"), [
+                 "password" => "testpass"
+             ])
+             ->assertJsonFragment([
                  "email" => ["The email field is required."],
-                 "password" => ["The password field is required."]
              ]);
     }
 
     /** @test */
-    public function successfull_login()
+    public function password_field_is_required_on_login()
     {
-        $user = factory("App\User")->create([
-            "name" => "testusername",
-            "email" => "valid@email.com",
-            "password" => Hash::make("testingpassword")
-        ]);
+        $this
+             ->json("POST", route("login"), [
+                 "email" => $this->root->email,
+             ])
+             ->assertJsonFragment([
+                 "password" => ["The password field is required."],
+             ]);
+    }
 
-        $response = $this->json("POST", route("login"), [
-            "email" => $user["email"],
-            "password" => "testingpassword"
-        ]);
-
-        $response->assertStatus(200);
+    /** @test */
+    public function successfull_login_returns_token()
+    {
+        $this->withoutExceptionHandling();
+        $response = $this
+            ->json("POST", route("login"), [
+                "email" => $this->admin->email,
+                "password" => "adminpass",
+            ]);
         $this->assertArrayHasKey("token", $response->json());
     }
 
