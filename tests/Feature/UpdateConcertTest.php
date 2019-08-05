2fd513019d943f98ad4f19b4982f4d084efd0451
ccDpE9 <z_eon00@protonmail.com>
2019-08-06 00:38:29 +0200

    Tests to determine whether authorized users can update fields that may need updating some time after the creation.

--- tests/Feature/UpdateConcertTest.php
+++ tests/Feature/UpdateConcertTest.php
@@ -20,16 +20,6 @@ class UpdateConcertTest extends TestCase
     }
 
     /** @test */
-    public function fillable_fields_can_be_updated()
-    {
-    }
-
-    /** @test */
-    public function guarded_fields_cant_be_updated()
-    {
-    }
-
-    /** @test */
     public function unauthenticated_users_cannot_update_concerts()
     {
         $this
@@ -61,6 +51,7 @@ class UpdateConcertTest extends TestCase
         ]);
     }
 
+
     /** @test */
     public function authenticated_root_user_can_update_concerts()
     {
@@ -76,6 +67,118 @@ class UpdateConcertTest extends TestCase
             ->assertJsonFragment(["status" => "Concert was successfully updated."]);
         $this->assertDatabaseHas("concerts", [
             "title" => "The end of the fucking world party."
+        ]);
+    }
+
+
+    // - Testing whether filleable fields can be updated, on by one, of cource
+    // @TODO: Isn't this unit testing, maybe?
+    
+    /** @test */
+    public function title_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "title" => "The end of the fucking world party."
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "title" => "The end of the fucking world party."
+        ]);
+    }
+
+    /** @test */
+    public function city_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "city" => "New York"
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "city" => "New York"
+        ]);
+    }
+
+    /** @test */
+    public function venue_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "venue" => "Trap House",
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "venue" => "Trap House",
+        ]);
+    }
+
+    /** @test */
+    public function venue_address_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "venue_address" => "Adress 99",
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "venue_address" => "Adress 99"
+        ]);
+    }
+
+    /** @test */
+    public function ticket_price_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "ticket_price" => "99.99"
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "ticket_price" => "99.99"
+        ]);
+    }
+
+    /** @test */
+    public function tickets_quantity_field_can_be_updated()
+    {
+        $this
+            ->withHeaders([
+                "Authorization" => "Bearer".$this->rootToken,
+                "Accept" => "application/json",
+            ])
+            ->put(route("concerts.update", $this->concert->id), [
+                "tickets_quantity" => "100"
+            ])
+            ->assertStatus(200)
+            ->assertJsonFragment(["status" => "Concert was successfully updated."]);
+        $this->assertDatabaseHas("concerts", [
+            "tickets_quantity" => "100"
         ]);
     }
 }
