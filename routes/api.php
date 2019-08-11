9800274a902c928e33ac47eeb59fd2c1baec6459
ccDpE9 <z_eon00@protonmail.com>
2019-08-11 20:23:33 +0200

    Fixed typo

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,14 @@
 use Illuminate\Http\Request;
 
 Route::namespace("Api")->group(function () {
-    Route::get("/concects/index", "ConcertController@index")->name("concerts.index");
+    Route::get("/concerts/index", "ConcertController@index")->name("concerts.index");
     Route::group([
         "middleware" => "jwt.verify",
     ], function() {
         Route::post("/concerts/store", "ConcertController@store")->name("concerts.store")->middleware("jwt.verify");
         Route::post("/concerts/show/{concert}", "ConcertController@show")->name("concerts.show")->middleware("jwt.verify");
         Route::put("/concerts/update/{concert}", "ConcertController@update")->name("concerts.update")->middleware("jwt.verify");
-        Route::delete("/concerts/update/{concert}", "ConcertController@destroy")->name("concerts.destroy")->middleware("jwt.verify");
+        Route::delete("/concerts/delete/{concert}", "ConcertController@destroy")->name("concerts.destroy")->middleware("jwt.verify");
     });
 });
 
