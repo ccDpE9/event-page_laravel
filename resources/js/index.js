671e9e7902cd8dcf31a407f39346b4228db7dd00
ccDpE9 <z_eon00@protonmail.com>
2019-08-12 16:28:01 +0200

    Cleaned up my index file by separating it from store creation.

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,22 +1,10 @@
 import React from "react";
 import { render } from "react-dom";
-
-import { 
-  createStore,
-  applyMiddleware
-} from "redux";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
 import reducer from "./reducers/index";
+import store from "./store";
 
 import App from "./components/App";
-
-const middleware = [ thunk ];
-
-const store = createStore(
-  reducer,
-  applyMiddleware(...middleware)
-);
 
 render(
   <Provider store={store}>
---
+++ resources/js/store/index.js
@@ -0,0 +1,15 @@
+import { 
+  createStore,
+  applyMiddleware
+} from "redux";
+import thunk from "redux-thunk";
+// import { routerMiddleware } from "connected-react-router";
+
+const middlewares = [ thunk ];
+
+const store = createStore(
+  reducer,
+  applyMiddleware(...middlewares)
+);
+
+export default store;
