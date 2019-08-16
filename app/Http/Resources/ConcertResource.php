d01d36a99b98b5ff7223bf460dfdd4cbceb89129
ccDpE9 <z_eon00@protonmail.com>
2019-08-16 22:27:44 +0200

    Concert Resource

---
+++ app/Http/Resources/ConcertResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ConcertResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            "date" => $this->date,
+            "city" => $this->city,
+            "venue" => $this->venue,
+            "tickets_price" => $this->tickets_price,
+        ];
+    }
+}
