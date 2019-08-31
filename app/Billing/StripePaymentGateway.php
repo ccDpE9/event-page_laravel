bd4a43e12d0b533a9793c1bcc1fb4ec3c465a7de
ccDpE9 <z_eon00@protonmail.com>
2019-08-31 15:01:29 +0200

    Just some modifications.

--- app/Billing/StripePaymentGateway.php
+++ app/Billing/StripePaymentGateway.php
@@ -3,17 +3,17 @@
 namespace App\Billing;
 
 use App\Billing\PaymentGateway;
-use Stripe\Charge;
-use Stripe\Error\InvalidRequest;
 use App\Billing\PaymentFailedException;
+
+use Stripe\Charge;
+use Stripe\Token;
+use Stripe\Error\InvalidRequest;
 
 class StripePaymentGateway implements PaymentGateway
 {
-    private $apiKey;
-
-    public function __construct($apiKey)
+    public function __construct()
     {
-        $this->apiKey = $apiKey;
+        \Stripe\Stripe::setApiKey(config("services.stripe.secret"));
     }
 
     public function charge($amount, $token)
@@ -23,9 +23,23 @@ class StripePaymentGateway implements PaymentGateway
                 "amount" => $amount,
                 "source" => $token,
                 "currency" => "usd"
-            ], ["api_key" => $this->apiKey]);
+            ]);
+            //], ["api_key" => $this->apiKey]);
         } catch (InvalidRequest $e) {
             throw new PaymentFailedException;
         }
+    }
+
+    public function testToken()
+    {
+        return Token::create([
+            "card" => [
+                "number" => "4242424242424242",
+                "exp_month" => 1,
+                "exp_year" => date("Y") + 1,
+                "cvc" => "123",
+            ],
+            //["api_key" => config("services.stripe.secret")]
+        ])->id;
     }
 }
