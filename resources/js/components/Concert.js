9787eb69d696443f6fa481bad7a4f0116b2118d9
ccDpE9 <z_eon00@protonmail.com>
2019-09-04 20:37:59 +0200

    Got my Concert Component css to an decenct form. Next thing to do is build Navbar with Cart Component in it.

--- resources/js/components/Concert.js
+++ resources/js/components/Concert.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
 
 export default class Concert extends Component {
+  onClick() {
+    retun (
+      // StripeForm
+    )
+  }
+
+  renderSoldOutConcert() {
+  }
+
   render() {
+    /*
+    if (this.props.data.ticketsLeft === 0) {
+      { renderSoldOutConcert() }
+    }
+    */
+
     return (
-      <div className="concert">
-        <div className="concert__title">Title</div>
-        <div className="concert_description"></div>
-        <div className="concert_date"></div>
+      <div className="concert-list__concert">
+        <div className="concert-list__concert-date">{ this.props.data.date }</div>
+        <div className="concert-list__concert-location">
+          <div className="concert-list__concert-city">{ this.props.data.city }</div>
+          <div className="concert-list__concert-avenue">{ this.props.data.avenue }</div>
+        </div>
+        <button
+          onClick={this.onClick}
+        >
+          Order
+        </button>
       </div>
     );
   }
 }
-
-
