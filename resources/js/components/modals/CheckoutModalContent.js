95e9a530fdc3af14da4eabec9e4f526d7bc6c16f
ccDpE9 <z_eon00@protonmail.com>
2019-10-01 20:13:36 +0200

    Connecting validation rules with custom Hook.

--- resources/js/components/modals/CheckoutModalContent.js
+++ resources/js/components/modals/CheckoutModalContent.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-import useForm from "./useForm";
 import {
   StripeProvider,
-  Elements
+  Elements,
+  CardElement
 } from "react-stripe-elements";
 
-const CheckoutModalContent = ({ onCheckoutClose, onCheckout }) => {
-  const { values, handleChange, handleSubmit } = useForm(charge);
+import useForm from "./useForm";
+import validate from "./CheckoutFormValidationRules";
+// @TODO: import order from "../../api/order";
 
-  function charge() {
+const CheckoutModalContent = ({ onCheckoutClose, onCheckout }) => {
+  const {
+    values,
+    errors,
+    handleChange,
+    handleSubmit
+  } = useForm(order, validate);
+
+  function order() {
     console.log(values);
   };
 
@@ -31,30 +39,51 @@ const CheckoutModalContent = ({ onCheckoutClose, onCheckout }) => {
                     value={values.name}
                     onChange={handleChange}
                   />
+                  {errors.name && (
+                    <p>{errors.name}</p>
+                  )}
                   <input type="text" name="email" className="checkout-form__input checkout-form__input-email" placeholder="Email" required
                     value={values.email}
                     onChange={handleChange}
                   />
+                  {errors.email && (
+                    <p>{errors.email}</p>
+                  )}
                   <input type="text" name="number" className="checkout-form__input checkout-form__input-number" placeholder="Phone number" required
                     value={values.number}
                     onChange={handleChange}
                   />
+                  {errors.number && (
+                    <p>{errors.number}</p>
+                  )}
                   <input type="text" name="country" className="checkout-form__input checkout-form__input-country" placeholder="Country" required
                     value={values.country}
                     onChange={handleChange}
                   />
+                  {errors.country && (
+                    <p>{errors.country}</p>
+                  )}
                   <input type="text" name="city" className="checkout-form__input checkout-form__input-city" placeholder="City" required
                     value={values.city}
                     onChange={handleChange}
                   />
+                  {errors.city && (
+                    <p>{errors.city}</p>
+                  )}
                   <input type="text" name="address"className="checkout-form__input checkout-form__input-address" placeholder="Street address" required
                     value={values.address}
                     onChange={handleChange}
                   />
+                  {errors.address && (
+                    <p>{errors.address}</p>
+                  )}
                   <input type="text" name="postal" className="checkout-form__input checkout-form__input-postal" placeholder="Postal code" required
                     value={values.postal}
                     onChange={handleChange}
                   />
+                  {errors.postal && (
+                    <p>{errors.postal}</p>
+                  )}
                   <CardElement />
                 </div>
               </div>
