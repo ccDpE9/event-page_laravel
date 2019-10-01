e949916dc8ccfc92df30266872e506cf063fc7e2
ccDpE9 <z_eon00@protonmail.com>
2019-10-01 16:05:17 +0200

    Previously, useEffect hook would run on initial render. This way, it's assured that it'll run only when form is submitted.

--- resources/js/components/modals/useForm.js
+++ resources/js/components/modals/useForm.js
@@ -4,15 +4,17 @@ const useForm = (callback, validate) => {
 
   const [values, setValues] = useState({});
   const [errors, setErrors] = useState({});
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   useEffect(() => {
-    if (Object.keys(errors).length === 0) {
+    if (Object.keys(errors).length === 0 && isSubmitting) {
       callback();
     }
   }, [errors]);
 
   const handleSubmit = (event) => {
     if (event) event.preventDefault();
+    setIsSubmitting(true);
     setErrors(validate(values));
   };
 
@@ -28,6 +30,7 @@ const useForm = (callback, validate) => {
     handleChange,
     handleSubmit,
     values,
+    errors
   }
 };
 
